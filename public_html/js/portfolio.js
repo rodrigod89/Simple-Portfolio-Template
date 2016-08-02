6f55a62ffac3862333cb03d6435be170a5a8b1f7
Rodrigo Diaz Bermudez <pericodiaz89@gmail.com>
2016-08-02 10:02:57 +0200

    Fixed the slider and it now works with Angular.

--- public_html/js/portfolio.js
+++ public_html/js/portfolio.js
@@ -1,9 +1,17 @@
 var app = angular.module('Portfolio', []);
+
+app.config(function ($sceDelegateProvider) {
+    $sceDelegateProvider.resourceUrlWhitelist([
+        'self',
+        'https://www.youtube.com/**'
+    ]);
+});
+
 app.controller('main', function ($scope) {
     $scope.name = "Name";
     $scope.profession = "Profession";
     $scope.intro = "This is the introduction of you and your portfolio, this section explains who you are and what you like in a very short paragraph.";
-    
+
     $scope.porfolio = [
         {
             name: "Project",
@@ -12,8 +20,23 @@ app.controller('main', function ($scope) {
             technology: "Technology 1, Technolog 2...",
             platform: "Android, IOS...",
             members: "Name A, Name B....",
-            main: "https://www.youtube.com/embed/ScMzIvxBSi4?rel=0&amp;showinfo=0",
-            img: "img/portfolio/",
+            main: "3bN_wOYm9ck",
+            img: "img/portfolio/project01/",
+            imgs: [
+                "placeholder.png",
+                "placeholder.png",
+                "placeholder.png"
+            ]
+        },
+        {
+            name: "Project",
+            description: "Description of the project and what it was about.",
+            role: "Role 1, Role 2...",
+            technology: "Technology 1, Technolog 2...",
+            platform: "Android, IOS...",
+            members: "Name A, Name B....",
+            main: "3bN_wOYm9ck",
+            img: "img/portfolio/project01/",
             imgs: [
                 "placeholder.png",
                 "placeholder.png",
@@ -21,4 +44,13 @@ app.controller('main', function ($scope) {
             ]
         }
     ];
+
+    $scope.getIframeSrc = function (src) {
+        return 'https://www.youtube.com/embed/' + src;
+    };
+
+//    UIkit.domObserve('uk-slider', function (element) {
+//      UIkit.component.boot('slider');
+//      UIkit.init(element);
+//    });
 });
