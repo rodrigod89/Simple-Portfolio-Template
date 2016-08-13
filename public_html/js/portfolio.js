893777b1b91b906a05691edf9d8600f68ce25e4c
Rodrigo Diaz Bermudez <pericodiaz89@gmail.com>
2016-08-13 20:48:32 +0200

    Finished first version of the porfolio

--- public_html/js/portfolio.js
+++ public_html/js/portfolio.js
@@ -10,8 +10,26 @@ app.config(function ($sceDelegateProvider) {
 app.controller('main', function ($scope) {
     $scope.name = "Name";
     $scope.profession = "Profession";
+    $scope.email = "youremail@email.com";
+    $scope.twitter = "username";
+    $scope.facebook = "username";
+    $scope.telephone = "+0000 000 000 000";
     $scope.intro = "This is the introduction of you and your portfolio, this section explains who you are and what you like in a very short paragraph.";
-
+    $scope.about = "Here you can talk in a bit more detail about the stuff you have done for the past years, what you like to do in general, where you want to do later on and even your work philosophy. \n\
+\n\
+The main idea is to give the reader a sense of who you are and where are you heading to. This should be a bit more space since well... we have it, thus we should be using it as much as we can.",
+    $scope.techSkills = [
+        "Skill A",
+        "Skill B", 
+        "Skill C", 
+        "Skill D"
+    ];
+    $scope.personalSkills = [
+        "Skill A",
+        "Skill B",
+        "Skill C",
+        "Skill D"
+    ];
     $scope.porfolio = [
         {
             name: "Project 1",
@@ -47,6 +65,16 @@ app.controller('main', function ($scope) {
             platform: "Android, IOS...",
             members: "Name A, Name B....",
             teaser_image: "img/portfolio/placeholder.png",
+            links: [
+                {
+                    icon: "external-link",
+                    url: "http://www.goldenfrogstudios.com/"
+                },
+                {
+                    icon: "youtube-play",
+                    url: "https://youtu.be/diEgroqiHeA"
+                }
+            ]
         },
         {
             name: "Project 3",
@@ -55,7 +83,7 @@ app.controller('main', function ($scope) {
             technology: "Technology 1, Technolog 2...",
             platform: "Android, IOS...",
             members: "Name A, Name B....",
-            teaser_image: "img/portfolio/placeholder.png",
+            teaser_image: "img/portfolio/placeholder.png"
         },
         {
             name: "Project 4",
@@ -64,7 +92,7 @@ app.controller('main', function ($scope) {
             technology: "Technology 1, Technolog 2...",
             platform: "Android, IOS...",
             members: "Name A, Name B....",
-            teaser_image: "img/portfolio/placeholder.png",
+            teaser_image: "img/portfolio/placeholder.png"
         },
         {
             name: "Project 5",
@@ -73,10 +101,19 @@ app.controller('main', function ($scope) {
             technology: "Technology 1, Technolog 2...",
             platform: "Android, IOS...",
             members: "Name A, Name B....",
-            teaser_image: "img/portfolio/placeholder.png",
+            teaser_image: "img/portfolio/placeholder.png"
         }
     ];
-
+    $scope.showProject = function (project) {
+        
+        var images = new Array();
+        project.imgs.forEach(function (e){
+            images.push({ 'source' : project.img + e, 'type': 'image' });
+        });
+        
+        var lightbox = UIkit.lightbox.create(images);
+        lightbox.show();
+    };
     $scope.getIframeSrc = function (src) {
         return 'https://www.youtube.com/embed/' + src;
     };
